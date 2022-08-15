0b5adf61b837707a7da10a68863db357c31fc510
khadidjamoraes <khadidjamoraes@gmail.com>
2022-08-14 21:46:23 -0300

    Create p29.js

---
+++ p29.js
@@ -0,0 +1,3 @@
+/*Aluna: Khadidja Moraes Lopes da Silva*/
+
+/*Escolhi o VScode, pois já tenho familiaridade com o software, além de possibilitar a utilização de diversas linguagens e pluggins em um só lugar.*/ 
